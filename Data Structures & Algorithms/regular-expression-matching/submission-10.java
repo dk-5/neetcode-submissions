01222852954ca76bfb7d6e01336a9010caf2e8b6
Dhruv Kothari <143609201+dk-5@users.noreply.github.com>
2026-07-31 12:49:37 +0530

    Add: regular-expression-matching - submission-10

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-10.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return dfs(0,0,s,p);
+    }
+    public boolean dfs(int i,int j,String s,String p)
+    {
+        if(i>=s.length() && j>=p.length())
+        {
+            return true;
+        }
+        if(j>=p.length())
+        {
+            return false;
+        }
+        boolean match=(i<s.length()) && (s.charAt(i)==p.charAt(j) || p.charAt(j)=='.') ;
+        if((j+1)<p.length() && p.charAt(j+1)=='*')
+        {
+            return (match && dfs(i+1,j,s,p)) || dfs(i,j+2,s,p);
+        }
+        if(match)
+        {
+            return dfs(i+1,j+1,s,p);
+        }
+        return false;
+    }
+}
