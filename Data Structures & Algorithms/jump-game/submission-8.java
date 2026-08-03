52d9d43e737cbd3a211f42bab2ad6b9e9a547423
Dhruv Kothari <143609201+dk-5@users.noreply.github.com>
2026-08-03 11:25:20 +0530

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.java
@@ -0,0 +1,32 @@
+class Solution {
+    // Boolean[] dp;
+    public boolean canJump(int[] nums) {
+      int goal=nums.length-1;
+      for(int i=nums.length-2;i>=0;i--)
+      {
+        int index=i+nums[i];
+        if(index>=goal)
+        {
+          goal=i;
+        }
+        
+      }
+      return goal==0;
+    }
+    // public boolean dfs(int i,int[] nums)
+    // {
+    //   if(i==nums.length-1) return true;
+    //   if(nums[i]==0) return false;
+    //   if(dp[i]!=null) return dp[i];
+    //   for(int j=1;j<=nums[i];j++)
+    //   {
+    //     if(dfs(i+j,nums))
+    //     {
+    //       dp[i]=true;
+    //       return true;
+    //     }
+    //   }
+    //   dp[i]=false;
+    //   return false;
+    // }
+}
